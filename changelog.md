ed3f8a6a2281b9b6a78bab134402d6753215cae8
OmgRod <89850217+OmgRod@users.noreply.github.com>
2026-02-17 18:36:31 +0000

    update to 2.2081

--- changelog.md
+++ changelog.md
@@ -1,3 +1,7 @@
+# v1.0.2
+
+- Update to 2.2081 (Geode v5)
+
 # v1.0.1
 
 - Small bugfix: Add space/enter key option for single-button popups
--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,25 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/FLAlertLayer.hpp>
-#include <geode.custom-keybinds/include/Keybinds.hpp>
+#include <Geode/loader/GameEvent.hpp>
+#include <Geode/loader/SettingV3.hpp>
 
 using namespace geode::prelude;
-using namespace keybinds;
-
-$execute {
-    BindManager::get()->registerBindable({
-        "btn1"_spr,
-        "Button 1",
-        "Popup button 1 keybind (typically for cancelling)",
-        { Keybind::create(KEY_C, Modifier::None) },
-        "Global/Popups"
-    });
-
-	BindManager::get()->registerBindable({
-        "btn2"_spr,
-        "Button 2",
-        "Popup button 2 keybind (typically for accepting something)",
-        { Keybind::create(KEY_Space, Modifier::None), Keybind::create(KEY_Enter, Modifier::None) },
-        "Global/Popups"
-    });
-}
 
 class $modify(MyFLAlertLayer, FLAlertLayer) {
 	bool init(FLAlertLayerProtocol* delegate, char const* title, gd::string desc, char const* btn1, char const* btn2, float width, bool scroll, float height, float textScale) {
 		if (!FLAlertLayer::init(delegate, title, desc, btn1, btn2, width, scroll, height, textScale)) return false;
 
-		this->template addEventListener<InvokeBindFilter>([&](InvokeBindEvent* event) {
-			if (event->isDown()) {
-				onBtn1(nullptr);
-				return ListenerResult::Stop;
-			}
-			return ListenerResult::Propagate;
-		}, "btn1"_spr);
+		this->addEventListener(KeybindSettingPressedEventV3(Mod::get(), "btn1"), [this](Keybind const& keybind, bool down, bool repeat, double timestamp) {
+            if (down && !repeat) {
+                onBtn1(nullptr);
+            }
+        });
 
-		if (btn2) {
-			this->template addEventListener<InvokeBindFilter>([&](InvokeBindEvent* event) {
-				if (event->isDown()) {
-					onBtn2(nullptr);
-					return ListenerResult::Stop;
-				}
-				return ListenerResult::Propagate;
-			}, "btn2"_spr);
-		} else {
-			this->template addEventListener<InvokeBindFilter>([&](InvokeBindEvent* event) {
-				if (event->isDown()) {
-					onBtn1(nullptr);
-					return ListenerResult::Stop;
-				}
-				return ListenerResult::Propagate;
-			}, "btn2"_spr);
-		}
+		this->addEventListener(KeybindSettingPressedEventV3(Mod::get(), "btn2"), [this](Keybind const& keybind, bool down, bool repeat, double timestamp) {
+            if (down && !repeat) {
+                onBtn2(nullptr);
+            }
+        });
 
 		return true;
 	}
